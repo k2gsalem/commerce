6681913a82477c4c7e306a98eb58709c3b1a718b
yuvaraj2016 <juvaraj2016@gmail.com>
2020-06-29 14:32:48 -0700

    suppliercat update

--- app/Entities/Config/ConfSupplierCat.php
+++ app/Entities/Config/ConfSupplierCat.php
@@ -4,7 +4,19 @@ namespace App\Entities\Config;
 
 use Illuminate\Database\Eloquent\Model;
 
+use OwenIt\Auditing\Auditable as AuditingAuditable;
+use OwenIt\Auditing\Contracts\Auditable;
+
+
 class ConfSupplierCat extends Model
 {
     //
+
+    use AuditingAuditable;
+
+    protected $fillable = [
+        'supplier_cat_desc','status_id', 'created_by', 'updated_by',
+    ];
+   
+
 }
