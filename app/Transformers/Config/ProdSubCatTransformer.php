1d5eacd98b5b8eed7bb802ae80a4973e3277572a
yuvaraj2016 <juvaraj2016@gmail.com>
2020-06-29 15:42:38 -0700

    Product Subcategory Update

---
+++ app/Transformers/Config/ProdSubCatTransformer.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Transformers\Config;
+
+use App\Entities\Config\ProdSubCat;
+use League\Fractal\TransformerAbstract;
+
+class ProdSubCatTransformer extends TransformerAbstract
+{
+    
+    public function transform(ProdSubCat $model)
+    {
+        
+        return [
+            'id' =>(int)$model->id,
+            'sub_category_short_code' =>(string)$model->sub_category_short_code,
+            'sub_category_desc' =>(string)$model->sub_category_desc,
+            'sub_category_image' =>(string)$model->sub_category_image,
+            'status_id'=>(int)$model->status_id, 
+            'created_at' => (string)$model->created_at->toIso8601String(),
+            'updated_by' => (string)$model->updated_at            
+            // 'created_by' => $model->created_by,
+            // 'updated_by' => $model->updated_by->toIso8601String(),
+            // 'created_at' => $model->created_at->toIso8601String(),
+        ];
+
+    }
+}
