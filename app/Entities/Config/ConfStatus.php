491054f3258a64d993536885b5a7507af4ea54c4
yuvaraj2016 <juvaraj2016@gmail.com>
2020-09-05 12:39:07 -0700

    update

--- app/Entities/Config/ConfStatus.php
+++ app/Entities/Config/ConfStatus.php
@@ -59,7 +59,7 @@ class ConfStatus extends Model implements Auditable
     }
     public function item()
     {
-        return $this->belongsToMany(Item::class,'status_id','conf_statuses','id');
+        return $this->belongsToMany(Item::class,'conf_statuses','status_id');
     }
     public function itemVariant()
     {
