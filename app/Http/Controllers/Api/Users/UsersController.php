adf7ea418cd2a577c05249bdcba58dd5e7cd4995
yuvaraj2016 <juvaraj2016@gmail.com>
2020-09-05 14:23:24 -0700

    sadsda

--- app/Http/Controllers/Api/Users/UsersController.php
+++ app/Http/Controllers/Api/Users/UsersController.php
@@ -127,7 +127,7 @@ class UsersController extends Controller
      
        
         $user = $this->model->byUuid($uuid)->firstOrFail();
-         return $user->roles();
+         return $user->roles()->toArray();
         // $user->delete();
 
         return $this->response->noContent();
