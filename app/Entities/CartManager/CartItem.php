5baf0bfc296f9123dac949aaa1dad387a3e1332e
yuvaraj2016 <juvaraj2016@gmail.com>
2020-09-05 13:37:06 -0700

    update

--- app/Entities/CartManager/CartItem.php
+++ app/Entities/CartManager/CartItem.php
@@ -34,7 +34,7 @@ class CartItem extends Model implements Auditable
     // }
     public function confStatus()
     {
-        return $this->belongsTo('App\Entities\Config\ConfStatus');
+        return $this->belongsTo('App\Entities\Config\ConfStatus','status_id');
     }
     public function cart()
     {
