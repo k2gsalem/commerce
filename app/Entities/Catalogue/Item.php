5115179a67d7905f6d46ba5f03d4486738286fd2
yuvaraj2016 <juvaraj2016@gmail.com>
2020-09-05 20:46:03 -0700

    dgf

--- app/Entities/Catalogue/Item.php
+++ app/Entities/Catalogue/Item.php
@@ -23,10 +23,7 @@ class Item extends Model implements Auditable,Searchable
 {
     use AuditingAuditable, SoftDeletes;
 
-    protected $casts = [
-        'min_order_amount' => 'float',
-    ];
-
+  
     protected $fillable = [
         'category_id',
         'sub_category_id',
