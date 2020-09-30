a2860e5f648bd22579e5d02917a9170c626c9872
k2gsalem <k2gsalem@gmail.com>
2020-09-30 21:42:45 +0530

    orders

---
+++ app/Http/Controllers/UserOrderItemController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Entities\Order\UserOrderItem;
+use Illuminate\Http\Request;
+
+class UserOrderItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Entities\Order\UserOrderItem  $userOrderItem
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserOrderItem $userOrderItem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Entities\Order\UserOrderItem  $userOrderItem
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserOrderItem $userOrderItem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Entities\Order\UserOrderItem  $userOrderItem
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserOrderItem $userOrderItem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Entities\Order\UserOrderItem  $userOrderItem
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserOrderItem $userOrderItem)
+    {
+        //
+    }
+}
---
+++ database/factories/UserOrderFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Entities\Order\UserOrder;
+use Faker\Generator as Faker;
+
+$factory->define(UserOrder::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
